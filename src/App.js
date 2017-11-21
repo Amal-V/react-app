6f4f7028f57bef3ac4c92deba7f226af59a7b858
Amal <amalchirackal@gmail.com>
2017-11-21 09:14:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header"> 
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React build with circle ci</h1>
+          <h1 className="App-title">Welcome to React build with circle ci and static webhosting</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
